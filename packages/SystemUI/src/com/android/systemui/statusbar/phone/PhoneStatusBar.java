9f4cbe4e2bd898ea8ab027f06e6fa4f74d99b05c
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-02-21 18:28:00 +0100

    QuickSettings: ﻿Add Tile general customizations
      * QS: Option to show four tiles per row (1/2) (Altaf Mahdi)
        This also reduces the size of the ripple when using 4 tiles per row
    
      * Squash of the following:
        QS: reduce TILE_ASPECT when using 4 tiles per row
        QS: don't resize dual tiles when using 4 per row
        QS: move all options into a SettingsObserver in QSPanel
    
      * Set number of tile columns [1/2] (bigrushdog)
        3, 4, or 5 columns. We don't want to get excessive here
    
      * We keep the Quick Settings panel clean by scaling some
        dimens using a factor determined by the column count
        In QSPanel, we scale:
        -- mCellHeight
        -- mCellWidth
        -- and add mColumns as a member
        In QSTileView, we scale:
        -- mIconSizePx
        -- mTilePaddingBelowIconPx
        -- R.dimen.qs_tile_text_size (dynamically generated)
        -- and add mSizeScale as a member
    
    Change-Id: I6fc8652ce3fe41521958de04a86e59a15f9f1445
    
      * PureNexus Edits: revert 4 tiles per row, and add main tiles to the observer
      * Fix Dual tiles in qs tuner (beanstown106)
        previously having show main tiles on showed 3/4/5 tiles on the top row and
        didnt reflect what was actually shown in the quick settings panel this was
        resolved by removing main tiles from the observer, also added a call to refresh
        all tiles on drop and on tiles changed as without these the top main tiles would
        loose their main tiles look in the tuner after a tile was changed, also remove
        all references to supportsDualTargets
    
      * Set default QS tiles (Alex Cruz)
        The defaults were being determine by the QSTileHost and if still showing 'default'
        it would read the 'quick_settings_tiles_default' string which includes all of our tiles
        https://github.com/DirtyUnicorns/android_frameworks_base/blob/m/packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java#L281-L306
    
    Change-Id: I7728f3e4911d3b98921fd33b210936a4140b13ed
    
      * Correct QS Tuner reset (camcory)
        Have the tiles reset to the new defined values in config "quick_settings_tiles"
    
    Change-Id: Ia7923030c5e5eb760766b1ff8a5525cc28114eed
    
      * Fix missing bits of QS tile customization
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
      * Rebased into one commit
    
    Signed-off-by: reS28raM <paolo.a.ulivi@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -436,6 +436,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POLICY_CONTROL), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.QS_NUM_TILE_COLUMNS), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -559,6 +562,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             if (mIconPolicy != null) {
                 mIconPolicy.setCurrentUserSetup(mUserSetup);
+            }
+
+            if (mQSPanel != null) {
+                mQSPanel.updateNumColumns();
             }
         }
     };
