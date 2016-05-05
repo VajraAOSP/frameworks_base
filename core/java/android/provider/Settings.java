858bd04cce68125430a95b50000916e8d414f8a6
TheMrcool212 <munirmohamadrani@gmail.com>
2016-05-05 23:43:00 +0200

    Add double tap to sleep on navigation bar [2/2]
      * PureNexus Edits:
        Made multiuser compatible
    
    Signed-off-by: reS28raM <paolo.a.ulivi@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3228,6 +3228,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         *  Enable navigation bar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
+
+        /**
          * Boolean value whether to link ringtone and notification volume
          * @hide
          */
